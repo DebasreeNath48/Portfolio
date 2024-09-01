3e743044a7774076d0cc9ca044562454cc27c780
DEBASREE NATH <119242606+DebasreeNath48@users.noreply.github.com>
2024-09-02 00:34:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const navLinks = document.querySelectorAll('nav a');
+
+navLinks.forEach(link => {
+  link.addEventListener('click', (event) => {
+    event.preventDefault();
+    const targetId = link.getAttribute('href').substring(1);
+    const targetElement = document.getElementById(targetId);
+    targetElement.scrollIntoView({ behavior: 'smooth' });
+  });
+});
